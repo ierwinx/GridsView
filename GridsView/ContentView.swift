64012a76e61208499e2577dd32c3e4bb5f31a118
Eяw!и Gontier <ierwinx@me.com>
2022-08-04 13:05:46 -0500

    Initial Commit

---
+++ GridsView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GridsView
+//
+//  Created by Erwin Luz León on 04/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GridsView/GridsViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridsViewApp.swift
+//  GridsView
+//
+//  Created by Erwin Luz León on 04/08/22.
+//
+
+import SwiftUI
+
+@main
+struct GridsViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
